1831add2c82852c2131a03ed98ead4e2c1b19042
Khinse6 <henri23.2003@gmail.com>
2024-01-29 02:30:44 +0000

    Update.html
    Overall look change in updates with some cool animations

--- calendar.js
+++ calendar.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const gridContainer = document.getElementById('grid-container');
+    const gridContainer = document.getElementById('calendar-container');
     const reverseButton = document.getElementById('reverse-button');
 
     // Initial grid items with specific content
     const initialGridItems = [
-        createGridItem('04/12/2023', '#', ['date-calendar', 'darken']),
-        createGridItem('11/12/2023', '#', ['date-calendar', 'darken']),
-        createGridItem('18/12/2023', '#', ['date-calendar', 'darken']),
-        createGridItem('25/12/2023', null, ['date-calendar', 'darken']),
-        createGridItem('01/01/2024', null, ['date-calendar', 'darken']),
-        createGridItem('08/01/2024', '#', ['date-calendar', 'darken'])
+        createGridItem('First Meetings', 'updates.html#update-start', ['date-calendar', 'darken']),
+        createGridItem('04/12/2023', 'updates.html#update1', ['date-calendar', 'darken']),
+        createGridItem('11/12/2023', 'updates.html#update2', ['date-calendar', 'darken']),
+        createGridItem('18/12/2023', 'updates.html#update3', ['date-calendar', 'darken']),
     ];
 
     // Append initial grid items to the grid container
---
+++ updates.js
@@ -0,0 +1,24 @@
+function fadeInOut() {
+    const elems = document.querySelectorAll('.update-container');
+  
+    elems.forEach((elem) => {
+      const elemTop = elem.getBoundingClientRect().top;
+      const elemBottom = elem.getBoundingClientRect().bottom;
+      const visibilityThreshold = window.innerHeight * 0.2;
+  
+      const fadeInOpacity = (visibilityThreshold - elemTop) / visibilityThreshold;
+      const fadeOutOpacity = (elemBottom - visibilityThreshold) / visibilityThreshold;
+  
+      const opacity = Math.min(Math.max(fadeInOpacity, 0), Math.min(fadeOutOpacity, 1));
+  
+      elem.style.transition = 'opacity .5s ease';
+      elem.style.opacity = opacity;
+    });
+  }
+  
+  function scrollFade() {
+    window.addEventListener('scroll', fadeInOut);
+  }
+  
+  document.addEventListener('DOMContentLoaded', scrollFade);
+  
