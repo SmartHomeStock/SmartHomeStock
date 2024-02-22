724b902d20e8f979b8b00d109a478c29bf1a42c8
Khinse6 <henri23.2003@gmail.com>
2024-02-22 11:39:18 +0000

    Organized Files

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,17 +1,19 @@
 const primaryNav = document.querySelector(".navbar-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener('click',() => {
+console.log("primaryNav:", primaryNav);
+console.log("navToggle:", navToggle);
+
+navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute("data-visible");
 
-    if(visibility === "false"){
+    console.log("visibility:", visibility);
+
+    if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
-    }
-
-    else{
+    } else {
         primaryNav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
-}
-);
+});
