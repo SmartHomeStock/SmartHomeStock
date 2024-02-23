f89fbd9f0678765a25734730c39b1d972a936301
Khinse6 <henri23.2003@gmail.com>
2024-02-23 23:47:21 +0000

    File managment

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Initial grid items with specific content
     const initialGridItems = [
-        createGridItem('First Meetings', 'updates.html#update-start', ['date-calendar', 'darken']),
-        createGridItem('04/12/2023', 'updates.html#update1', ['date-calendar', 'darken']),
-        createGridItem('11/12/2023', 'updates.html#update2', ['date-calendar', 'darken']),
-        createGridItem('18/12/2023', 'updates.html#update3', ['date-calendar', 'darken']),
-        createGridItem('26/02/2024', 'updates.html#update4', ['date-calendar', 'darken']),
+        createGridItem('First Meetings','pages/updates.html#update-start', ['date-calendar', 'darken']),
+        createGridItem('04/12/2023', 'pages/updates.html#update1', ['date-calendar', 'darken']),
+        createGridItem('11/12/2023', 'pages/updates.html#update2', ['date-calendar', 'darken']),
+        createGridItem('18/12/2023', 'pages/updates.html#update3', ['date-calendar', 'darken']),
+        createGridItem('26/02/2024', 'pages/updates.html#update4', ['date-calendar', 'darken']),
     ];
 
     // Append initial grid items to the grid container
